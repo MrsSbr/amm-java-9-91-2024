95cf406019909570ffd02020a1f3d701a2d959a8
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2024-09-11 17:59:44 +0300

    golukovich_lab0

--- app/G9/golukovich.timofey/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/golukovich.timofey/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -1,5 +1,9 @@
 package ru.vsu.amm.java;
 
 public class Main {
-  public static void main(String[] args) { System.out.println("Hello world!"); }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    StringBuilder builder = new StringBuilder("Hello, ");
+    System.out.println(builder.append("Main!"));
+  }
 }
