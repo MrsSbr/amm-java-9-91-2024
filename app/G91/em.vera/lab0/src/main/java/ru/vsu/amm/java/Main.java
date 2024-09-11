726c531fa5c38e6627d19f3b491e61654c4533b8
em_vera <em_vera@mail.ru>
2024-09-11 22:23:11 +0300

    lab0

---
+++ app/G91/em.vera/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -0,0 +1,9 @@
+package ru.vsu.amm.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i ++)
+            System.out.println("Hello world!");
+    }
+}
