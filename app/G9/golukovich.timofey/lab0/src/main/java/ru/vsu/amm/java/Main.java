e9df10457cfed90d150f199fe1742e605b094dfa
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2024-09-11 17:31:25 +0300

    golukovich_lab0

---
+++ app/G9/golukovich.timofey/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -0,0 +1,5 @@
+package ru.vsu.amm.java;
+
+public class Main {
+  public static void main(String[] args) { System.out.println("Hello world!"); }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,6 +28,10 @@ student("nelezin.oleg", G9) {
     lab(0)
 }
 
+student("golukovich.timofey", G9) {
+    lab(0)
+}
+
 // DSL для подпроектов [Не трогать]
 includeProject("template-lab0", file("template/lab0"))
 
