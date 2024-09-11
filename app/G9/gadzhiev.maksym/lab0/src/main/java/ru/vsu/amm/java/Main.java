9007da8d2cf9fa63e7014f0877da658c0092e0af
maksymka-gadzhiev <MaKsy1M@yandex.ru>
2024-09-11 21:10:44 +0300

    gadzhiev_lab0

---
+++ app/G9/gadzhiev.maksym/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -0,0 +1,10 @@
+package ru.vsu.amm.java;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println(a + b);
+        System.out.println("Hello world!");
+    }
+}
