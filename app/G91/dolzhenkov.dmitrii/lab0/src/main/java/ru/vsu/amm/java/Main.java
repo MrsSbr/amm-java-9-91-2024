cc45bd64fdce4dde215ffc7858d662832fed6f58
dolmitya <dolzhenkov_d_a@mail.ru>
2024-09-11 21:45:01 +0300

    lab0 test

--- app/G91/dolzhenkov.dmitrii/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G91/dolzhenkov.dmitrii/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -2,7 +2,7 @@ package ru.vsu.amm.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!1");
 
         System.out.println(3);
     }
