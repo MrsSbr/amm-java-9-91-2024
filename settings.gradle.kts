2718a664ad2f66cd4e461120315a92bd65cfce38
_chromium_ <146010522+HromBromIod@users.noreply.github.com>
2024-09-12 17:46:58 +0300

    golukovich_lab1_update4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,19 +19,11 @@ student("sbrodova.daria", G91) {
     lab(0)
 }
 
-student("kurkina.tanya", G91) {
-    lab(0)
-}
-
 student("belozerov.alexei", G91) {
     lab(0)
 }
 
 student("gonnykh.alexander", G91) {
-    lab(0)
-}
-
-student("ulyanov.leonid", G91) {
     lab(0)
 }
 
