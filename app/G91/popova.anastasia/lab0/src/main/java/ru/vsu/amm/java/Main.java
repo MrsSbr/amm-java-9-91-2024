a9555761b6ea5cca2c55883d0c1a96998f9a0cef
Anastasia <littlementalpain@mail.ru>
2024-09-12 22:31:23 +0300

    "lab0 successfully done! c:"

--- app/G91/popova.anastasia/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G91/popova.anastasia/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -2,7 +2,7 @@ package ru.vsu.amm.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!\n");
+
         System.out.println("we will never sleep");
         System.out.println("'cause sleep is for the weak");
     }
