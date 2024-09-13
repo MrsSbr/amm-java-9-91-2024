403279010c1f4f69fd2f31a4a7f390daa0b751f9
andrey <gepeg3317@mail.ru>
2024-09-13 18:03:42 +0300

    aa

--- app/G9/anikandrov.andrey/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/anikandrov.andrey/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -2,6 +2,6 @@ package ru.vsu.amm.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hey ho!");
+        System.out.println("Hey ho!!");
     }
 }
