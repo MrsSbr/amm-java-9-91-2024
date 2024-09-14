44700f7193708ae3ab1b3e975fa5b3319e2f74cc
IO-oINik <nik.mokshin.2004@mail.ru>
2024-09-14 13:58:16 +0300

    lab0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -66,6 +66,10 @@ student("barkov.pavel", G9) {
     lab(0)
 }
 
+student("mokshin.nikita", G9) {
+    lab(0)
+}
+
 // DSL для подпроектов [Не трогать]
 includeProject("template-lab0", file("template/lab0"))
 
