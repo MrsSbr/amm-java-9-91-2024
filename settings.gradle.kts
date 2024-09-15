49ed3d8b7a62867f2dcd4bfacbfa08fd0959f048
Polina <kisspoli@yandex.ru>
2024-09-15 23:11:25 +0300

    Add Serova Polina

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -87,6 +87,10 @@ student("garshin.maxim", G9) {
     lab(0)
 }
 
+student("serova.polina", G9) {
+    lab(0)
+}
+
 // DSL для подпроектов [Не трогать]
 includeProject("template-lab0", file("template/lab0"))
 
