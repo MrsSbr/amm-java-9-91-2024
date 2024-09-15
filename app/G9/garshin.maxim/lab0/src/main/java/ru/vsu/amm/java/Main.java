cee370bfe6a5f7bc72170f0760463f9efa84a866
Max1mGarsh1n <maximgarshin@gmail.com>
2024-09-15 10:00:19 +0300

    garshin_maxim_lab0

--- app/G9/garshin.maxim/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/garshin.maxim/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -2,7 +2,7 @@ package ru.vsu.amm.java;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i=0;i<5;++i) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(i);
         }
     }
