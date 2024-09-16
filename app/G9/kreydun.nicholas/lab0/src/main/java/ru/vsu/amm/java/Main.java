4b8b0b9c9cc1f4966c7b4d4e92114e152927a0b8
kreydun.nicholas <koley-2015@mail.ru>
2024-09-16 19:02:57 +0300

    .

--- app/G9/kreydun.nicholas/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/kreydun.nicholas/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -2,6 +2,6 @@ package ru.vsu.amm.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello world!");
     }
 }
---
+++ app/G9/kreydun.nicholas/settings.gradle.kts
@@ -0,0 +1,80 @@
+import Settings_gradle.Group.*
+
+rootProject.name = "-amm-java-9-91-2024"
+
+
+student("sbrodov.andrey", G2_2) {
+    lab(0)
+}
+
+student("sbrodova.daria", G9) {
+    lab(0)
+}
+
+student("kireev.dmitrii", G91) {
+    lab(0)
+}
+
+student("sbrodova.daria", G91) {
+    lab(0)
+}
+
+student("kurkina.tanya", G91) {
+    lab(0)
+}
+
+student("belozerov.alexei", G91) {
+    lab(0)
+}
+
+student("gonnykh.alexander", G91) {
+    lab(0)
+}
+
+student("ulyanov.leonid", G91) {
+    lab(0)
+}
+
+student("nelezin.oleg", G9) {
+    lab(0)
+}
+
+student("golukovich.timofey", G9) {
+    lab(0)
+}
+
+student("dolzhenkov.dmitrii", G91) {
+    lab(0)
+}
+student("kreydun.nicholas", G9) {
+    lab(0)
+}
+
+// DSL для подпроектов [Не трогать]
+includeProject("template-lab0", file("template/lab0"))
+
+enum class Group { G9, G91, G2_2 }
+
+fun student(name: String, group: Group, block: LabsAppender.() -> Unit = {}) {
+    LabsAppender(name, group).apply(block)
+}
+
+class LabsAppender(private val student: String, private val group: Group) {
+    fun lab(labNum: Int) {
+        val path = file("app/$group/$student/lab$labNum")
+        val title = "$student-lab$labNum"
+        path.mkdirs()
+        includeProject(
+            title = title,
+            path = path
+        )
+    }
+}
+
+fun includeProject(title: String, path: File) {
+    include(title)
+    project(":$title").apply {
+        this.projectDir = file(path)
+        name = title
+    }
+}
