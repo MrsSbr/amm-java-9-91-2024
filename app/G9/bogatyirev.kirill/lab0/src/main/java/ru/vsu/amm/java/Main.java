66f2fe78135da120c1765a6033bc119648b0d595
NonsencePluto <kirill210804@mail.ru>
2024-09-18 20:12:14 +0300

    добавил новую строчку

--- app/G9/bogatyirev.kirill/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/bogatyirev.kirill/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -1,8 +1,7 @@
 package ru.vsu.amm.java;
 
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hello, world");
         System.out.println("Test");
     }
