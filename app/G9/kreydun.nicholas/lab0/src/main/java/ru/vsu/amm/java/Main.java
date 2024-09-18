3df67494018bdcaea485237c2becdda3ef4ec741
kreydun.nicholas <koley-2015@mail.ru>
2024-09-18 16:22:07 +0300

    .

--- app/G9/kreydun.nicholas/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/kreydun.nicholas/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -1,7 +1,55 @@
 package ru.vsu.amm.java;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
+    public static int jump(List<Integer> nums) {
+        int n = nums.size();
+        int[] dp = new int[n];
+         Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= i + nums.get(i) && j < n; j++) {
+                dp[j] = Math.min(dp[j], dp[i] + 1);
+            }
+        }
+
+        return dp[n - 1];
+    }
+
+    public static List<Integer> arrInit() {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> arr = new ArrayList<>();
+        System.out.println("Введите числа (для завершения введите '.'):");
+        boolean Flag = true;
+        while (Flag) {
+            if (scanner.hasNextInt()) {
+                arr.add(scanner.nextInt());
+            } else {
+                String input = scanner.next();
+                if (input.equalsIgnoreCase(".")) {
+                    Flag = false;
+                } else {
+                    throw new IllegalArgumentException("Неверный ввод: " + input);
+                }
+            }
+        }
+
+        return arr;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            List<Integer> nums = arrInit();
+            int minJumps = jump(nums);
+            System.out.println("Минимальное количество прыжков: " + minJumps);
+        }catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
