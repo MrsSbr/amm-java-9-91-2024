e2fd8f04e0eebffd63ccbf5445d05a102b9f4f45
nastushkazv <nastushkazv@mail.ru>
2024-09-19 14:03:58 +0300

    добавила изменения в код

--- app/G9/zayseva.anastasiya/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/zayseva.anastasiya/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Java!");
-        System.out.println("Anastasiya");
+        System.out.println("Anastasiya!");
     }
 }
