f8ddf99aa65964ce05e8f3111bf30376250449c3
ozeiroz <fursovgggg@gmail.com>
2024-09-19 14:03:38 +0300

    lab0

--- app/G9/globuchik.dmitry/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/globuchik.dmitry/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -2,9 +2,13 @@ package ru.vsu.amm.java;
 
 public class Main {
     public static void main(String[] args) {
-        int before = 0, current = 1;
+        int before = 0;
+        int current = 1;
+
         for (int i = 1; i <= 10; ++i) {
+            int tmp = before + current;
             System.out.print(i);
+
             if (i <= 3) {
                 if (i == 1) {
                     System.out.print("-st ");
@@ -14,10 +18,11 @@ public class Main {
                     System.out.print("-rd ");
                 }
             }
-            else
+            else {
                 System.out.print("-th ");
+            }
+
             System.out.print("Fibonacci number is: " + (before + current) + '\n');
-            int tmp = before + current;
             before = current;
             current = tmp;
         }
