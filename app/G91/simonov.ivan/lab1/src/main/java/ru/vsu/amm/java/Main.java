7bd25aa9eb0f2045e5bafca8978c951113ae7f71
Ivan Simonov <ivansimonov7@yandex.ru>
2024-09-20 23:48:09 +0300

    checkBloacks added

---
+++ app/G91/simonov.ivan/lab1/src/main/java/ru/vsu/amm/java/Main.java
@@ -0,0 +1,95 @@
+package ru.vsu.amm.java;
+
+
+public class Main {
+
+    final static int LENGTH = 9;
+    final static int BLOCK = 3;
+
+    public static boolean checkRow(char [][] board, int length, int i, char val) {
+        boolean isFound = false;
+        for (int k = 0; k < length; k++) {
+            if (board[i][k] == val) {
+                isFound = true;
+                break;
+            }
+        }
+        return !isFound;
+    }
+
+    public static boolean checkColumn(char [][] board, int length, int j, char val) {
+        boolean isFound = false;
+        for (int k = 0; k < length; k++) {
+            if (board[k][j] == val) {
+                isFound = true;
+                break;
+            }
+        }
+        return !isFound;
+    }
+
+    public static boolean checkBlocks(char [][] board, int length, int block, int row, int col, char val) {
+        boolean isFound = false;
+        for (int i = row - block + 1; i < (row + block); i++) {
+            for (int j = col - block + 1; j < (col + block); j++) {
+                if (i >= 0 && i < length && j >= 0 && j < length && board[i][j] == val) {
+                    isFound = true;
+                    break;
+                }
+            }
+        }
+        return !isFound;
+    }
+
+    public static boolean sudokuSolver(char [][] board, int length, int block) {
+        boolean isPossible = true;
+        char digit;
+        for (int i = 0; i < length && isPossible; i++) {
+            for (int j = 0; j < length && isPossible; j++) {
+                if (board[i][j] == '.') {
+                    isPossible = false;
+                    for (int k = 0; k < length; k++) {
+                        digit = Character.forDigit(k + 1, 10);
+                        if (checkRow(board, length, i, digit)
+                        && checkColumn(board, length, j, digit)
+                        && checkBlocks(board, block, length, i, j, digit)) {
+                            isPossible = true;
+                            board[i][j] = digit;
+                            break;
+                        }
+                    }
+                }
+                printSudokuSolvation(board, length);
+            }
+        }
+        return isPossible;
+    }
+
+    public static void printSudokuSolvation(char [][] board, int length) {
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+    }
+    public static void main(String[] args) {
+       char[][] board = {{'5','3','.','.','7','.','.','.','.'},
+                        {'6','.','.','1','9','5','.','.','.'},
+                        {'.','9','8','.','.','.','.','6','.'},
+                        {'8','.','.','.','6','.','.','.','3'},
+                        {'4','.','.','8','.','3','.','.','1'},
+                        {'7','.','.','.','2','.','.','.','6'},
+                        {'.','6','.','.','.','.','2','8','.'},
+                        {'.','.','.','4','1','9','.','.','5'},
+                        {'.','.','.','.','8','.','.','7','9'}};
+
+       if (sudokuSolver(board, LENGTH, BLOCK)) {
+           printSudokuSolvation(board, LENGTH);
+       }
+       else {
+           System.out.println("No solvation!");
+       }
+    }
+}
