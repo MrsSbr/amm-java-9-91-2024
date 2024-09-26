4d2973cae299baa96c8cfb7d593f0bc2f8978c4b
CuriCara <Dark77_77@mail.ru>
2024-09-26 08:41:58 +0300

    lab1 done

---
+++ app/G9/bogdanov.nikita/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -0,0 +1,11 @@
+package ru.vsu.amm.java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int s = 12;
+        System.out.println(s);
+    }
+}
