4d5fd243afc04ea0ccad51314482ac947675bdb6
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2024-09-26 20:56:06 +0300

    lab2_update4

--- app/G9/golukovich.timofey/lab2/src/main/java/ru/vsu/amm/java/Classes/Triceratops.java
+++ app/G9/golukovich.timofey/lab2/src/main/java/ru/vsu/amm/java/Classes/Triceratops.java
@@ -24,4 +24,34 @@ public class Triceratops extends Dinosaur {
         return "Трицератопс - Возраст: " + age + ", Имя: " + name
                 + ", Место обитания: " + habitat + ", Длинна рогов: " + hornsLength;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Triceratops)) {
+            return false;
+        }
+        Triceratops triceratops = (Triceratops) o;
+        boolean isNumbersEquals = age == triceratops.age
+                && hornsLength == triceratops.hornsLength;
+        boolean isNamesEquals = (name == null && triceratops.name == null
+                || name != null && name.equals(triceratops.name));
+        boolean isHabitatsEquals = (habitat == null && triceratops.habitat == null
+                || habitat != null && habitat.equals(triceratops.habitat));
+        return isNumbersEquals && isNamesEquals && isHabitatsEquals;
+    }
+
+    @Override
+    public final int hashCode() {
+        int result = age + hornsLength;
+        for (var c : name.toCharArray()) {
+            result += c % 24;
+        }
+        for (var c : habitat.toCharArray()) {
+            result += c % 42;
+        }
+        return result;
+    }
 }
