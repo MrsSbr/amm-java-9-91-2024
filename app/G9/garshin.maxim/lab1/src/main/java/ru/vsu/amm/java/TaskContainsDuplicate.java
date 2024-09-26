c7e8cd984bb5f452e51a49f095574aaf9bfd9356
Max1mGarsh1n <maximgarshin@gmail.com>
2024-09-26 09:33:41 +0300

    garshin_maxim_corrected_lab1

---
+++ app/G9/garshin.maxim/lab1/src/main/java/ru/vsu/amm/java/TaskContainsDuplicate.java
@@ -0,0 +1,46 @@
+package ru.vsu.amm.java;
+import ru.vsu.amm.java.Checks.ArrayLimitations;
+
+import java.util.*;
+
+public class TaskContainsDuplicate {
+    public static boolean hasDuplicateElements(int[] numbers) {
+        Set<Integer> uniqueElements = new HashSet<>();
+        for (int number : numbers) {
+            if (!uniqueElements.contains(number)) {
+                uniqueElements.add(number);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            ArrayLimitations arrayLimitations = new ArrayLimitations();
+
+            System.out.print("Enter the length of the array: ");
+            int arrayLength = input.nextInt();
+            arrayLimitations.checkArrayLengthOrThrow(arrayLength);
+
+            int[] nums = new int[arrayLength];
+
+            for (int i = 0; i < arrayLength; ++i) {
+                System.out.printf("Enter next element %d: ", i + 1);
+                int element = input.nextInt();
+                arrayLimitations.checkArrayElementValueOrThrow(element);
+                nums[i] = element;
+            }
+
+            System.out.print("Input: [");
+            for (int i = 0; i < nums.length; ++i) {
+                System.out.print(nums[i] + (i == nums.length-1 ? "]" : ", "));
+            }
+
+            System.out.print("\nOutput: " + hasDuplicateElements(nums));
+        } catch (IllegalArgumentException | InputMismatchException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
