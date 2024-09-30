00abadf004d2d2ef8ed63a0bc1b6f83ee96ec499
vikat <vikatpk2004@gmail.com>
2024-09-30 20:12:23 +0300

    tupikova_vika_lab0

--- app/G9/tupikova.viktoria/lab0/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/tupikova.viktoria/lab0/src/main/java/ru/vsu/amm/java/Main.java
@@ -2,6 +2,6 @@ package ru.vsu.amm.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bye, world!");
+        System.out.println("Bye world!");
     }
 }
