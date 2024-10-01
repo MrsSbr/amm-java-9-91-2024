938c5026c30aad973646b55397d7402c9bc6ecdb
dolmitya <dolzhenkov_d_a@mail.ru>
2024-10-01 22:55:25 +0300

    redact QueenAnt.java and override equals

--- app/G91/dolzhenkov.dmitrii/lab2/src/main/java/ru/vsu/amm/java/AntWorkingMain.java
+++ app/G91/dolzhenkov.dmitrii/lab2/src/main/java/ru/vsu/amm/java/AntWorkingMain.java
@@ -7,9 +7,9 @@ import ru.vsu.amm.java.classes.WorkerAnt;
 
 public class AntWorkingMain {
     public static void main(String[] args) {
-        AbstractAnt worker = new WorkerAnt("Муравей 1", 2);
+        AbstractAnt worker = new WorkerAnt("Королева", 5);
         AbstractAnt cook = new SoldierAnt("Муравей 2", 3);
-        AbstractAnt queen = new QueenAnt("Королева", 5);
+        AbstractAnt queen = new QueenAnt("Королева", 5, 4);
 
 
         worker.work();
@@ -24,6 +24,13 @@ public class AntWorkingMain {
         if (queen instanceof QueenAnt) {
             System.out.println("Это королева муравей.");
         }
+
+        System.out.println(queen.equals(worker)); //false
+
+        QueenAnt anotherQueen = new QueenAnt("Королева", 5, 4);
+        System.out.println(queen.equals(anotherQueen)); //true
+        anotherQueen.makeLarvae();
+        System.out.println(queen.equals(anotherQueen)); //false
 
         System.out.println(worker.toString());
     }
--- app/G91/dolzhenkov.dmitrii/lab2/src/main/java/ru/vsu/amm/java/classes/QueenAnt.java
+++ app/G91/dolzhenkov.dmitrii/lab2/src/main/java/ru/vsu/amm/java/classes/QueenAnt.java
@@ -4,13 +4,19 @@ public class QueenAnt extends AbstractAnt {
 
     private int countLarvaes = 0;
 
-    public QueenAnt(String name, int age) {
+    public QueenAnt(String name, int age, int countLarvaes) {
         super(name, age);
+        this.countLarvaes = countLarvaes;
     }
 
     @Override
     public void work() {
         System.out.println(name + " царствует в муравейнике.");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj) && countLarvaes == ((QueenAnt) obj).countLarvaes;
     }
 
     public void makeLarvae() {
