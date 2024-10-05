42f2fe613125a97df7280617332a5915e546200c
daniel.jasser <dzhasser@yandex.ru>
2024-10-06 00:05:51 +0300

    lab1 changes

--- app/G9/jasser.daniel/lab1/src/main/java/ru/vsu/amm/java/CoinChange.java
+++ app/G9/jasser.daniel/lab1/src/main/java/ru/vsu/amm/java/CoinChange.java
@@ -5,30 +5,34 @@ import java.util.Scanner;
 
 
 public class CoinChange {
+
     public static void main(String[] args) {
+        int answer = coinChange();
+        if (answer == -1) {
+            System.out.println("This amount of money can't be represented by any combination of coins.");
+        } else {
+            System.out.println("This amount of money can be represented by a combination of " + answer + " coins.");
+        }
+    }
+
+    static int coinChange(){
         Scanner in = new Scanner(System.in);
 
         System.out.println("Enter the number of coins (no more than 12): ");
-        int amountVariations = in.nextInt();
-
-        amountVariations = Help.checkRange(1, 12, amountVariations);
+        int amountVariations = Help.initValue(1, 12);
 
         int[] coins = new int[amountVariations];
 
         for (int i = 0; i < amountVariations; i++) {
             System.out.println("Enter the coin value: ");
-            coins[i] = in.nextInt();
-            coins[i] = Help.checkRange(1, Integer.MAX_VALUE, coins[i]);
+            coins[i] = Help.initValue(1, Integer.MAX_VALUE);
         }
 
-        Arrays.sort(coins);
-
         System.out.println("Enter the amount you want to change: ");
-        int amount = in.nextInt();
-        amount = Help.checkRange(1, 10000, amount);
+        int amount = Help.initValue(1, 10000);
 
-        int answer = tryToChange(coins, amount, 0, amountVariations - 1);
-        System.out.println(answer);
+        Arrays.sort(coins); // Использует timsort сортировку (сложность в лучщем случае n, в среднем и худшем nlogn)
+        return tryToChange(coins, amount, 0, amountVariations - 1);
     }
 
     static int tryToChange(int[] coins, int amount, Integer countOfCoins, int numberOfCoins) {
@@ -39,8 +43,9 @@ public class CoinChange {
                 if (amount >= 0) {
                     amount = amount - coins[numberOfCoins];
                     countOfCoins++;
-                    if (amount == 0)
+                    if (amount == 0) {
                         endOfChange = true;
+                    }
                 } else {
                     amount += coins[numberOfCoins];
                     countOfCoins--;
@@ -48,8 +53,9 @@ public class CoinChange {
                 }
             }
 
-            if (numberOfCoins == 0 && amount > 0)
+            if (numberOfCoins == 0 && amount > 0) {
                 return -1;
+            }
 
             return (amount == 0) ?
                 countOfCoins : tryToChange(coins, amount, countOfCoins, numberOfCoins - 1);
--- app/G9/jasser.daniel/lab1/src/main/java/ru/vsu/amm/java/Help.java
+++ app/G9/jasser.daniel/lab1/src/main/java/ru/vsu/amm/java/Help.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class Help {
     private static Scanner in = new Scanner(System.in);
 
-    static int checkRange(int left, int right, int variable) {
+    static int initValue(int left, int right) {
+        System.out.format("*input the number in the range from %s to %s \n", left, right);
+        int variable = in.nextInt();
         while (variable < left || variable > right) {
-            System.out.format("The number must be in the range from %s to %s: \n", left, right);
+            System.out.format("*the number must be in the range from %s to %s \n", left, right);
             variable = in.nextInt();
         }
         return variable;
