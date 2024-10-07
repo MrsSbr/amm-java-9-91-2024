6579f2cdb6f884ebbae7ab5bc10e1b06a4f77071
Nastia Saf <safonovaanastasia2004@gmail.com>
2024-10-07 22:12:32 +0300

    Сафонова Настя, исправила 1 лабу

--- app/G9/safonova.nastia/lab1/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/safonova.nastia/lab1/src/main/java/ru/vsu/amm/java/Main.java
@@ -1,21 +1,18 @@
 package ru.vsu.amm.java;
 
-public class Main
-{
-  public static int countLitBulbs(int n)
-  {
-    if (n < 0)
-    {
+public class Main {
+  public static int countLitBulbs(int n) {
+    if (n < 0) {
       return 0;
     }
-    return (int) Math.sqrt(n); // Возвращаем кол-во квадратов чисел, которые <= n
+    return (int) Math.sqrt(n);
   }
 
   public static void main(String[] args) {
-    System.out.println(countLitBulbs(3));  // Output: 1
-    System.out.println(countLitBulbs(0));  // Output: 0
-    System.out.println(countLitBulbs(1));  // Output: 1
-    System.out.println(countLitBulbs(7)); // Output: 2
-    System.out.println(countLitBulbs(50)); // Output: 7
+    System.out.println(countLitBulbs(3));
+    System.out.println(countLitBulbs(0));
+    System.out.println(countLitBulbs(1));
+    System.out.println(countLitBulbs(7));
+    System.out.println(countLitBulbs(50));
   }
 }
