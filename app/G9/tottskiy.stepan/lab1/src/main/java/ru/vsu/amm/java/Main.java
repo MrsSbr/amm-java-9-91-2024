f9bf26ae75c9408877af6180f9fd18740c49e0c3
MRSPEEDWAGOON <totsky.f@yandex.ru>
2024-10-12 17:31:13 +0300

    lab0

--- app/G9/tottskiy.stepan/lab1/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/tottskiy.stepan/lab1/src/main/java/ru/vsu/amm/java/Main.java
@@ -1,26 +1,24 @@
 package ru.vsu.amm.java;
 
-
-
 class Solution {
-public int searchInsert(int[] nums, int target) {
-    if (nums.length == 0) {
-        return 0;
-    }
-    int left = 0;
-    int right = nums.length - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) {
-            return mid;
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length == 0) {
+            return 0;
         }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
-    return left;
-}
 
     public static void main(String[] args) {
         Solution solution = new Solution();
