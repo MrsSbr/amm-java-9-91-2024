3e2064c59e6b241df096bdbe351191cc2a3ea53c
Leonid <ulyanov.leo.v@gmail.com>
2024-10-21 01:57:52 +0300

    created tests (work in progress)

--- app/G91/ulyanov.leonid/lab3/src/test/src/main/java/ru/vsu/amm/java/ServiceTests.java
+++ app/G91/ulyanov.leonid/lab3/src/test/src/main/java/ru/vsu/amm/java/ServiceTests.java
@@ -1,7 +1,116 @@
 package ru.vsu.amm.java;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.vsu.amm.java.entity.SongPlayback;
+import ru.vsu.amm.java.enums.Genre;
+import ru.vsu.amm.java.service.MusicService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ServiceTests {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private List<SongPlayback> songPlaybacks;
+
+    @BeforeEach
+    public void start() {
+        songPlaybacks = generateList();
+    }
+
+    @Test
+    public void amountOfRecentStreamsTest() {
+        int amount = MusicService.getAmountOfRecentStreams(songPlaybacks,
+                "title2", "artist2", Genre.POP);
+        assertEquals(1, amount);
+    }
+
+    @Test
+    public void userStreamingDataTest() {
+        Map<String, Set<String>> streamingData = MusicService.getUserStreamingData(songPlaybacks);
+        assertEquals(new HashMap<String, Set<String>>() {
+            {
+                put("user1", new HashSet<>() {{
+                    add("title1 by artist1, CLASSICAL");
+                    add("title2 by artist2, POP");
+                }});
+                put("user2", new HashSet<>() {{
+                    add("title3 by artist3, INDIE");
+                    add("title4 by artist4, EXPERIMENTAL");
+                }});
+                put("user3", new HashSet<>() {{
+                    add("title5 by artist5, ROCK");
+                    add("title6 by artist6, INDIE");
+                }});
+            }
+        }, streamingData);
+    }
+
+    @Test
+    public void userStreamingDataLeastPopularTest() {
+        Map<String, Set<String>> streamingData = MusicService.getUserStreamingDataLeastPopular(songPlaybacks);
+        assertEquals(new HashMap<String, Set<String>>() {
+            {
+                put("user1", new HashSet<>() {{
+                    add("title1 by artist1, CLASSICAL");
+                }});
+                put("user2", new HashSet<>() {{
+                    add("title4 by artist4, EXPERIMENTAL");
+                }});
+                put("user3", new HashSet<>() {{
+                    add("title5 by artist5, ROCK");
+                    add("title6 by artist6, INDIE");
+                }});
+            }
+        }, streamingData);
+    }
+
+    @Test
+    public void userStreamingDataMostFavoriteTest() {
+        Map<String, String> streamingData = MusicService.getUserStreamingDataMostFavorite(songPlaybacks);
+        assertEquals(new HashMap<String, String>() {
+            {
+                put("user1", "title1 by artist1, CLASSICAL");
+                put("user2", "title3 by artist3, INDIE");
+                put("user3", "title5 by artist5, ROCK");
+            }
+        }, streamingData);
+    }
+
+    public static List<SongPlayback> generateList() {
+        return new ArrayList<>(List.of(
+                new SongPlayback("user1",
+                        "title1", "artist1",
+                        Genre.CLASSICAL, LocalDate.of(2020, 9, 15)),
+                new SongPlayback("user1",
+                        "title2", "artist2",
+                        Genre.POP, LocalDate.of(2024, 10, 15)),
+                new SongPlayback("user1",
+                        "title1", "artist1",
+                        Genre.CLASSICAL, LocalDate.of(2023, 9, 12)),
+                new SongPlayback("user2",
+                        "title3", "artist3",
+                        Genre.INDIE, LocalDate.of(2024, 10, 15)),
+                new SongPlayback("user3",
+                        "title5", "artist5",
+                        Genre.ROCK, LocalDate.of(2023, 10, 15)),
+                new SongPlayback("user2",
+                        "title3", "artist3",
+                        Genre.INDIE, LocalDate.of(2020, 10, 15)),
+                new SongPlayback("user3",
+                        "title5", "artist5",
+                        Genre.ROCK, LocalDate.of(2020, 10, 15)),
+                new SongPlayback("user3",
+                        "title6", "artist6",
+                        Genre.INDIE, LocalDate.of(2020, 10, 15)),
+                new SongPlayback("user2",
+                        "title4", "artist4",
+                        Genre.EXPERIMENTAL, LocalDate.of(2020, 10, 15))));
     }
 }
