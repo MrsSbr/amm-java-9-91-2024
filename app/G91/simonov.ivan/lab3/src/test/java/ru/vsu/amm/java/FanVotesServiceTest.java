e80f938756b2f5cfb77450bba044bdd1cbd80a7b
Симонов Иван <ivansimonov7@yandex.ru>
2024-10-26 19:02:00 +0300

    Added and checked Tests for all players mentioned collection

--- app/G91/simonov.ivan/lab3/src/test/java/ru/vsu/amm/java/FanVotesServiceTest.java
+++ app/G91/simonov.ivan/lab3/src/test/java/ru/vsu/amm/java/FanVotesServiceTest.java
@@ -12,6 +12,8 @@ import java.util.stream.IntStream;
 
 public class FanVotesServiceTest {
     private static final FanVotesService avgFanVotesService = fillAverageFanVotes();
+    private static final FanVotesService allPlayersMentionedFanVotesService =
+            fillAllPlayersMentionedFanVotes();
     private final static int MIN_VOTE = avgFanVotesService.getMinVote();
     private final static int MAX_VOTE = avgFanVotesService.getMaxVote();
 
@@ -27,6 +29,21 @@ public class FanVotesServiceTest {
         return avgService;
     }
 
+    public static FanVotesService fillAllPlayersMentionedFanVotes() {
+        FanVotesService avgService = new FanVotesService();
+        avgService.setFanVote(List.of(
+                new FanVote(Set.of(1, 2, 3)),
+                new FanVote(Set.of(4, 5, 6)),
+                new FanVote(Set.of(7, 8, 9)),
+                new FanVote(Set.of(10, 11, 12)),
+                new FanVote(Set.of(13, 14, 15)),
+                new FanVote(Set.of(16, 17, 18)),
+                new FanVote(Set.of(19, 20, 21)),
+                new FanVote(Set.of(22))
+        ));
+        return avgService;
+    }
+
     @Test
     public void testFindMostPopularPlayersBasedOnAverageCollection() {
         Assertions.assertEquals(Set.of(2, 3),
@@ -34,7 +51,7 @@ public class FanVotesServiceTest {
     }
 
     @Test
-    public void testFindPlayersWithoutVotes() {
+    public void testFindPlayersWithoutVotesBasedOnAverageCollection() {
         Set<Integer> expectedCollection = new HashSet<>(Set.of(4, 7, 9));
         expectedCollection.addAll(IntStream.rangeClosed(11, MAX_VOTE)
                 .boxed()
@@ -44,8 +61,34 @@ public class FanVotesServiceTest {
     }
 
     @Test
-    public void testFindVotedPlayers() {
+    public void testFindVotedPlayersBasedOnAverageCollection() {
         Assertions.assertEquals(Set.of(1, 2, 3, 5, 6, 8, 10),
                 avgFanVotesService.findVotedPlayers());
+    }
+
+    @Test
+    public void testFindMostPopularPlayersBasedOnAllPlayersMentionedCollection() {
+        Set<Integer> expectedCollection =
+                new HashSet<>(IntStream.rangeClosed(MIN_VOTE, MAX_VOTE)
+                .boxed()
+                .toList());
+        Assertions.assertEquals(expectedCollection,
+                allPlayersMentionedFanVotesService.findMostPopularPlayers());
+    }
+
+    @Test
+    public void testFindPlayersWithoutVotesBasedOnAllPlayersMentionedCollection() {
+        Assertions.assertEquals(Set.of(),
+                allPlayersMentionedFanVotesService.findPlayersWithoutVotes());
+    }
+
+    @Test
+    public void testFindVotedPlayersBasedOnAllPlayersMentionedCollection() {
+        Set<Integer> expectedCollection =
+                new HashSet<>(IntStream.rangeClosed(MIN_VOTE, MAX_VOTE)
+                        .boxed()
+                        .toList());
+        Assertions.assertEquals(expectedCollection,
+                allPlayersMentionedFanVotesService.findVotedPlayers());
     }
 }
