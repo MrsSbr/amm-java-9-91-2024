254141f26e9c7b62cb16e270c726cbf421511f6d
Oleg Nelezin <astravsu@gmail.com>
2024-10-30 20:45:22 +0300

    some adds

--- app/G9/nelezin.oleg/lab5/src/main/java/ru/vsu/amm/java/Context.java
+++ app/G9/nelezin.oleg/lab5/src/main/java/ru/vsu/amm/java/Context.java
@@ -1,4 +1,44 @@
 package ru.vsu.amm.java;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Context {
+
+    private Map<Class<?>, Object> beans = new HashMap<>();
+
+    public Context() {
+
+    }
+
+    private void init(Class<?>... clazz) {
+        for (Class<?> cl : clazz) {
+            try {
+                Object instance = cl.getDeclaredConstructor().newInstance();
+                beans.put(cl, instance);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void initAutowiredFields(Object instance) {
+        Field[] fields = instance.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Autowired.class)) {
+                try {
+                    field.setAccessible(true);
+                    field.set(instance, getBean(field.getType()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public <T> T getBean(Class<T> tClass) {
+        return (T) beans.get(tClass);
+    }
 }
