229039b1ab7a80d225be3901a79bb4c92698b797
Oleg Nelezin <astravsu@gmail.com>
2024-10-31 00:10:06 +0300

    it works

--- app/G9/nelezin.oleg/lab5/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/nelezin.oleg/lab5/src/main/java/ru/vsu/amm/java/Main.java
@@ -1,7 +1,10 @@
 package ru.vsu.amm.java;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Context context = new Context();
+        Service2 service2 = context.getBean(Service2.class);
+        service2.goodMethod();
     }
 }
--- app/G9/nelezin.oleg/lab5/src/main/java/ru/vsu/amm/java/Service2.java
+++ app/G9/nelezin.oleg/lab5/src/main/java/ru/vsu/amm/java/Service2.java
@@ -2,4 +2,8 @@ package ru.vsu.amm.java;
 
 @Component
 public class Service2 {
+
+    public void goodMethod() {
+        System.out.println("I'm good!");
+    }
 }
