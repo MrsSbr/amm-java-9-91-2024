98e306f94183fe9e6bd384838e93bb918f02431a
Viktoria Shipilova <v.yu.shipilova@gmail.com>
2024-11-05 15:39:19 +0300

    Viktoria Shipilova. Added lab 4. Added logging.

--- app/G9/shipilova.viktoria/lab4/src/main/java/ru/vsu/amm/java/sports/medals/DataHandler.java
+++ app/G9/shipilova.viktoria/lab4/src/main/java/ru/vsu/amm/java/sports/medals/DataHandler.java
@@ -2,22 +2,42 @@ package ru.vsu.amm.java.sports.medals;
 
 import java.io.*;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class DataHandler {
-    public void saveToFale(String path) {
+    private static final Logger logger = Logger.getLogger(DataHandler.class.getName());
+
+    static {
+        try {
+            FileHandler fileHandler = new FileHandler("app/G9/shipilova.viktoria/lab4/medals_rec.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Could not initialize file handler for logger", e);
+        }
+    }
+
+    public void saveToFile(String path) {
+        logger.log(Level.INFO, "Saving medals to " + path);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
             List<Medal>  medals = MedalFactory.generateMedal();
             for (Medal medal : medals) {
                 writer.write(String.format("%s;%s;%s;%d%n", medal.getCountry(), medal.getKindOfSport(), medal.getAthlete(), medal.getPlace()));
             }
+            logger.log(Level.INFO, "Saving completed successfully.");
         } catch (IOException ex) {
+            logger.log(Level.SEVERE, "Error saving medals.", ex);
             throw new RuntimeException(ex);
         }
     }
 
-    public List<Medal> loadFromFale(String path) {
+    public List<Medal> loadFromFile(String path) {
+        logger.log(Level.INFO, "Loading medals from " + path);
         try (BufferedReader reader = new BufferedReader(new FileReader(path))){
-            return reader.lines()
+            List<Medal> medals = reader.lines()
                     .map(line -> {
                         String[] parts = line.split(";");
                         Country country = Country.valueOf(parts[0]);
@@ -26,7 +46,10 @@ public class DataHandler {
                         int place = Integer.parseInt(parts[3]);
                         return new Medal(country, kindOfSport, athlete, place);
                     }).toList();
+            logger.log(Level.INFO, "Saving completed successfully.");
+            return medals;
         } catch (IOException ex){
+            logger.log(Level.SEVERE, "Error loading medals.", ex);
             throw new RuntimeException(ex);
         }
     }
--- app/G9/shipilova.viktoria/lab4/src/main/java/ru/vsu/amm/java/sports/medals/MedalApplication.java
+++ app/G9/shipilova.viktoria/lab4/src/main/java/ru/vsu/amm/java/sports/medals/MedalApplication.java
@@ -1,16 +1,40 @@
 package ru.vsu.amm.java.sports.medals;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class MedalApplication {
     private static final String PATH = "app/G9/shipilova.viktoria/lab4/medals.txt";
+    private static Logger logger = Logger.getLogger(MedalApplication.class.getName());
+
+    static {
+        try {
+            FileHandler fileHandler = new FileHandler("app/G9/shipilova.viktoria/lab4/medals_app.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Could not initialize file handler for logger", e);
+        }
+    }
 
     public static void main(String[] args) {
+        Logger rootLogger = Logger.getLogger("");
+        rootLogger.setLevel(Level.INFO);
+        for (var handler : rootLogger.getHandlers()) {
+            handler.setLevel(Level.INFO);
+        }
+
+        logger.log(Level.INFO, "Starting MedalApplication.");
         DataHandler dataHandler = new DataHandler();
-        dataHandler.saveToFale(PATH);
-        List<Medal> medals = dataHandler.loadFromFale(PATH);
+        dataHandler.saveToFile(PATH);
+        List<Medal> medals = dataHandler.loadFromFile(PATH);
         MedalAnalyzer medalAnalyzer = new MedalAnalyzer();
 
+        logger.log(Level.INFO, "Analyzing medals...");
         System.out.println("\nTop 3 countries in the medal standings:");
         System.out.println(medalAnalyzer.topThreeInMedals(medals));
         System.out.println("\nMedalists in sports:");
@@ -22,5 +46,6 @@ public class MedalApplication {
         } else {
             System.out.println("There is no outstanding athlete. Several athletes have the same number of medals.\n");
         }
+        logger.log(Level.INFO, "Application finished.");
     }
 }
