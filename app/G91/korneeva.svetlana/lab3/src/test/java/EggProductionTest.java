9d756792bb82c094bc17d123ef0bc7ece63650d9
sveto4kaBBY <svetkorneeva0404@gmail.com>
2024-11-14 08:30:07 +0300

    fixed

--- app/G91/korneeva.svetlana/lab3/src/test/java/EggProductionTest.java
+++ app/G91/korneeva.svetlana/lab3/src/test/java/EggProductionTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class EggProductionTest {
+public final class EggProductionTest {
 
     @Test
     public void testFindBirdTypesWithHighProduction() {
