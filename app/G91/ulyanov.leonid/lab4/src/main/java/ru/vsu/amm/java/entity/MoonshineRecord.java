69a99d8dbbf266fdeec1c3f205995e808b24d6b6
Ulyanov Leonid <ulyanov.leo.v@gmail.com>
2024-11-21 23:05:15 +0300

    implemented MoonshineService.java

--- app/G91/ulyanov.leonid/lab4/src/main/java/ru/vsu/amm/java/entity/MoonshineRecord.java
+++ app/G91/ulyanov.leonid/lab4/src/main/java/ru/vsu/amm/java/entity/MoonshineRecord.java
@@ -1,10 +1,10 @@
 package ru.vsu.amm.java.entity;
 
-import ru.vsu.amm.java.enums.Ingredients;
+import ru.vsu.amm.java.enums.Ingredient;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public record MoonshineRecord(LocalDate date, String label, List<Ingredients> ingredients,
+public record MoonshineRecord(LocalDate date, String label, List<Ingredient> ingredients,
                               float volume, float makingTime) {
 }
--- app/G91/ulyanov.leonid/lab4/src/main/java/ru/vsu/amm/java/service/MoonshineService.java
+++ app/G91/ulyanov.leonid/lab4/src/main/java/ru/vsu/amm/java/service/MoonshineService.java
@@ -1,4 +1,86 @@
 package ru.vsu.amm.java.service;
 
+import ru.vsu.amm.java.entity.MoonshineRecord;
+import ru.vsu.amm.java.enums.Ingredient;
+
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MoonshineService {
+    public static Map<Ingredient, Double> getAverageTimeForIngredients(List<MoonshineRecord> drinksList)
+            throws NullPointerException {
+        if (drinksList == null) {
+            throw new NullPointerException();
+        }
+
+        HashMap<Ingredient, List<Float>> ingredientsTime = new HashMap<>();
+
+        Arrays.stream(Ingredient.values())
+                .forEach(ingredient -> ingredientsTime.put(ingredient, new ArrayList<>()));
+
+        drinksList
+                .forEach(x -> x.ingredients()
+                        .forEach(ingredient -> ingredientsTime.get(ingredient).add(x.makingTime())));
+
+        return ingredientsTime.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        x -> x.getValue()
+                                .stream()
+                                .mapToDouble(Float::doubleValue)
+                                .average()
+                                .orElse(0.0)));
+    }
+
+    public static int getMaxIngredientsMonth(List<MoonshineRecord> drinksList)
+            throws NullPointerException {
+        if (drinksList == null) {
+            throw new NullPointerException();
+        }
+
+        HashMap<Integer, List<Ingredient>> monthsIngredients = new HashMap<>();
+
+        drinksList.forEach(x -> {
+            int month = x.date().getMonthValue();
+            if (!monthsIngredients.containsKey(month)) {
+                monthsIngredients.put(month, new ArrayList<>());
+            } else {
+                monthsIngredients.put(month,
+                        Stream.concat(monthsIngredients.get(month).stream(),
+                                x.ingredients().stream()).toList());
+            }
+        });
+
+        return monthsIngredients.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, x -> (long) x.getValue().size()))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .orElse(Map.entry(-1, -1L))
+                .getKey();
+    }
+
+    public static Map<String, Float> getTotalVolumeForEachDrink(List<MoonshineRecord> drinksList)
+            throws NullPointerException {
+        if (drinksList == null) {
+            throw new NullPointerException();
+        }
+        HashMap<String, Float> drinksMap = new HashMap<>();
+        drinksList.forEach(x -> {
+            if (!drinksMap.containsKey(x.label())) {
+                drinksMap.put(x.label(), x.volume());
+            } else {
+                drinksMap.put(x.label(), drinksMap.get(x.label()) + x.volume());
+            }
+        });
+        return drinksMap;
+    }
 }
