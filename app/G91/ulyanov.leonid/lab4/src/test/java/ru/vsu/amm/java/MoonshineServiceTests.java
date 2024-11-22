da01d5287ae99b971f2f15d7418558f06f3e533f
Ulyanov Leonid <ulyanov.leo.v@gmail.com>
2024-11-22 03:28:07 +0300

    java code convention 2

--- app/G91/ulyanov.leonid/lab4/src/test/java/ru/vsu/amm/java/MoonshineServiceTests.java
+++ app/G91/ulyanov.leonid/lab4/src/test/java/ru/vsu/amm/java/MoonshineServiceTests.java
@@ -13,7 +13,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MoonshineServiceTests {
     private List<MoonshineData> drinks;
