f91d199d1f5e9c03175403ad20400af9db56c719
CuriCara <dark77_77@mail.ru>
2024-11-28 10:47:25 +0300

    lab3 changes

--- app/G9/bogdanov.nikita/lab3/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/bogdanov.nikita/lab3/src/main/java/ru/vsu/amm/java/Main.java
@@ -1,20 +1,20 @@
 package ru.vsu.amm.java;
 
-import static ru.vsu.amm.java.classes.Demographics.*;
+import ru.vsu.amm.java.classes.Demographics;
+import ru.vsu.amm.java.classes.Generator;
 import ru.vsu.amm.java.classes.Student;
-import static ru.vsu.amm.java.classes.Generator.*;
-import java.util.*;
+import java.util.List;
 
 
 
 public class Main {
     public static void main(String[] args) {
-        List<Student> students = generateStud(1200);
-        List<int[]> studBirthPerMonth = studPerMonth(students);
+        List<Student> students = Generator.generateStud(1200);
+        List<int[]> studBirthPerMonth = Demographics.studPerMonth(students);
         for (int[] stud : studBirthPerMonth) {
             System.out.printf("Month: %d; Male - %d; Female - %d%n", stud[0], stud[1], stud[2]);
         }
-        List<Integer> monthWhenFemaleMore = femaleMoreThenMale(students);
+        List<Integer> monthWhenFemaleMore = Demographics.femaleMoreThenMale(students);
         System.out.printf("%nMonth, when female more birth then male - %s%n", monthWhenFemaleMore);
     }
 }
