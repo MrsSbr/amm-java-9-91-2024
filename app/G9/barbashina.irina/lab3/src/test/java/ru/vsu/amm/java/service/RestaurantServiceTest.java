d3f2053a2f38d1d20c02f58954e2255a40eae364
irisic12 <barbasinairina341@gmail.com>
2024-11-28 08:36:39 +0300

    изолировала тесткейсы

--- app/G9/barbashina.irina/lab3/src/test/java/ru/vsu/amm/java/service/RestaurantServiceTest.java
+++ app/G9/barbashina.irina/lab3/src/test/java/ru/vsu/amm/java/service/RestaurantServiceTest.java
@@ -14,41 +14,11 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static ru.vsu.amm.java.fortest.GetSomeOrders.getSomeOrders;
 
 public class RestaurantServiceTest {
     private RestaurantService restaurantService;
     private List<Order> orders;
-
-
-    private static List<Order> getSomeOrders() {
-        Dish dish1 = Dish.builder()
-                .dish(TypeDish.BORCH)
-                .ingredients(new ArrayList<>(Arrays.asList(Ingredients.BEET, Ingredients.CABBAGE)))
-                .price(100)
-                .build();
-
-        Dish dish2 = Dish.builder()
-                .dish(TypeDish.KARBONARA)
-                .ingredients(new ArrayList<>(Arrays.asList(Ingredients.PASTA, Ingredients.CREAM)))
-                .price(200)
-                .build();
-
-        Dish dish3 = Dish.builder()
-                .dish(TypeDish.CHARLOTTE)
-                .ingredients(new ArrayList<>(Arrays.asList(Ingredients.APPLE, Ingredients.FLOUR)))
-                .price(300)
-                .build();
-
-        Order order1 = Order.builder()
-                .dishes(new ArrayList<>(Arrays.asList(dish1,dish2)))
-                .build();
-
-        Order order2 = Order.builder()
-                .dishes(new ArrayList<>(Arrays.asList(dish3)))
-                .build();
-
-        return Arrays.asList(order1,order2);
-    }
 
     @BeforeEach
     public void setup() {
