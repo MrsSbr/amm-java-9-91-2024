9014777ffa7f555ed4959dfad9e4b8ae6aadb544
aleckbb <Belozerov200@gmail.com>
2024-11-28 09:54:37 +0300

    updated lab4

--- app/G91/belozerov.alexei/lab4/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G91/belozerov.alexei/lab4/src/main/java/ru/vsu/amm/java/Main.java
@@ -5,6 +5,7 @@ import ru.vsu.amm.java.reader.Reader;
 import ru.vsu.amm.java.service.ShipService;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,23 +15,22 @@ public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
     private static final String PATH = "app/G91/belozerov.alexei/lab4/src/main/java/ru/vsu/amm/java/resources/ships.txt";
 
-    public static void main(String[] args) throws IOException {
-        List<Ship> ships;
+    public static void main(String[] args) {
         try {
             Reader reader = new Reader();
-            ships = reader.read(PATH);
+            List<Ship> ships = reader.read(PATH);
+
+            System.out.println("Amount of ships boarded by nationality: "
+                    + ShipService.boundingShipCountByNationality(ships) + '\n');
+
+            System.out.println("Less profit month: "
+                    + ShipService.lessProfitMonth(ships).toString() + '\n');
+
+            System.out.println("Most rums stocks for last 3 years: "
+                    + ShipService.mostRumsStocks(ships) + '\n');
         } catch (IOException e) {
-            logger.log(Level.SEVERE, "Can't read file: " + PATH + "\n");
-            throw e;
+            logger.log(Level.SEVERE, Arrays.toString(e.getStackTrace()));
+            System.out.println("Can't read file: " + PATH + "\n");
         }
-
-        System.out.println("Amount of ships boarded by nationality: "
-                + ShipService.boundingShipCountByNationality(ships) + '\n');
-
-        System.out.println("Less profit month: "
-                + ShipService.lessProfitMonth(ships).toString() + '\n');
-
-        System.out.println("Most rums stocks for last 3 years: "
-                + ShipService.mostRumsStocks(ships) + '\n');
     }
 }
