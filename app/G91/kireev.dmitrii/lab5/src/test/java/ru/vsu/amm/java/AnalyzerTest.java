e6a6737d68f48d9dc9ef186326ed3e1547e9063f
1lostevil1 <ki-04@yandex.ru>
2024-12-10 20:58:01 +0300

    some new tests

--- app/G91/kireev.dmitrii/lab5/src/test/java/ru/vsu/amm/java/AnalyzerTest.java
+++ app/G91/kireev.dmitrii/lab5/src/test/java/ru/vsu/amm/java/AnalyzerTest.java
@@ -2,14 +2,15 @@ package ru.vsu.amm.java;
 
 import org.junit.jupiter.api.Test;
 import ru.vsu.amm.java.Analyzer.ClassSizeAnalyzer;
+import ru.vsu.amm.java.SizeType.SizeType;
 import ru.vsu.amm.java.TestClasses.TestClassWithBitSizeType;
 import ru.vsu.amm.java.TestClasses.TestClassWithByteSizeType;
+import ru.vsu.amm.java.TestClasses.TestClassWithKilobyteSizeType;
 import ru.vsu.amm.java.TestClasses.TestClassWithoutAnnotation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnalyzerTest {
-
 
     @Test
     public void testAnalyzeClassSizeWithBitSizeType() {
@@ -20,8 +21,15 @@ public class AnalyzerTest {
 
     @Test
     public void testAnalyzeClassSizeWithByteSizeType() {
-        double expectedSize = (Integer.SIZE + Byte.SIZE + Character.SIZE) / 8.0;
+        double expectedSize = (Integer.SIZE + Byte.SIZE + Character.SIZE) / (1.0*SizeType.BYTE.getSizeInBits());
         double actualSize = ClassSizeAnalyzer.analyzeClassSize(TestClassWithByteSizeType.class);
+        assertEquals(expectedSize, actualSize, 0.001);
+    }
+
+    @Test
+    public void testAnalyzeClassSizeWithKilobyteSizeType() {
+        double expectedSize = (Integer.SIZE + Byte.SIZE + Character.SIZE) / (1.0*SizeType.KILOBYTE.getSizeInBits());
+        double actualSize = ClassSizeAnalyzer.analyzeClassSize(TestClassWithKilobyteSizeType.class);
         assertEquals(expectedSize, actualSize, 0.001);
     }
 
