819a5e86023660fb1e406a8947c37efac496e2f7
CuriCara <dark77_77@mail.ru>
2024-12-12 20:47:33 +0300

    Lab3 changes

--- app/G9/bogdanov.nikita/lab3/src/test/java/DemographicsTest.java
+++ app/G9/bogdanov.nikita/lab3/src/test/java/DemographicsTest.java
@@ -47,6 +47,34 @@ public class DemographicsTest {
     }
 
     @Test
+    public void testStudPerMonthFalse() {
+        List<Student> students = List.of(
+                new Student("Ivan", Gender.Male, 1),
+                new Student("Anna", Gender.Female, 1),
+                new Student("Oleg", Gender.Male, 2),
+                new Student("Sveta", Gender.Female, 2),
+                new Student("Dima", Gender.Male, 3)
+        );
+
+        List<int[]> result = Demographics.studPerMonth(students);
+
+        assertEquals(12, result.size());
+
+        int[] january = result.get(0);
+        int[] february = result.get(1);
+        int[] march = result.get(2);
+
+        assertEquals(0, january[1]);
+        assertEquals(0, january[2]);
+
+        assertEquals(1, february[1]);
+        assertEquals(0, february[2]);
+
+        assertEquals(0, march[1]);
+        assertEquals(1, march[2]);
+    }
+
+    @Test
     public void testFemaleMoreThenMale() {
         List<Student> students = List.of(
                 new Student("Ivan", Gender.Male, 1),
@@ -59,5 +87,19 @@ public class DemographicsTest {
 
         assertFalse(result.contains(1));
         assertTrue(result.contains(2));
+    }
+
+    @Test
+    public void testFemaleMoreThenMaleFalse() {
+        List<Student> students = List.of(
+                new Student("Ivan", Gender.Male, 1),
+                new Student("Anna", Gender.Female, 1),
+                new Student("Oleg", Gender.Male, 2),
+                new Student("Sveta", Gender.Female, 2),
+                new Student("Ira", Gender.Female, 2)
+        );
+        List<Integer> result = Demographics.femaleMoreThenMale(students);
+
+        assertTrue(result.contains(1));
     }
 }
