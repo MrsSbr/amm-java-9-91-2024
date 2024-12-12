682d3fa7682564548339c71a0eb10f6e97b27131
Necrobos <mishagavrilow@gmail.com>
2024-12-12 23:55:12 +0300

    Update build.gradle.kts

--- app/G9/gavrilov.mihail/lab3/build.gradle.kts
+++ app/G9/gavrilov.mihail/lab3/build.gradle.kts
@@ -11,12 +11,8 @@ repositories {
 
 dependencies {
     implementation("org.testng:testng:7.1.0")
-    implementation("org.testng:testng:7.1.0")
     implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     implementation("junit:junit:4.13.1")
-    implementation("org.testng:testng:7.1.0")
-    implementation("junit:junit:4.13.1")
-    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
