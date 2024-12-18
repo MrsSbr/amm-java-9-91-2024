08b0010004a3f711e5c2e799ce9ef5a3d317d3a4
Nastia Saf <safonovaanastasia2004@gmail.com>
2024-12-18 13:28:35 +0300

    lab 4

--- app/G9/safonova.nastia/lab4/src/main/java/ru/vsu/amm/java/CoffeeGenerator.java
+++ app/G9/safonova.nastia/lab4/src/main/java/ru/vsu/amm/java/CoffeeGenerator.java
@@ -3,20 +3,28 @@ package ru.vsu.amm.java;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class CoffeeGenerator {
+    private static final Logger log;
+
+    static {
+        log = Logger.getLogger(CoffeeGenerator.class.getName());
+    }
+
     private static final Sort[] SORTS = Sort.values();
 
     private static final ProcessingType[] PROCESSING_TYPES =
             ProcessingType.values();
 
     private static final String[] COUNTRIES = {"Brazil", "Columbia", "Vietnam",
-    "Ethiopia", "Kenya", "Guatemala", "India"};
+            "Ethiopia", "Kenya", "Guatemala", "India"};
 
     private static final String[] FARMS = {"Farm 1", "Farm 2", "Farm 3",
             "Farm 4", "Farm 5"};
 
     public static List<Producer> generateProducerList(int count) {
+        log.info("Method generateProducerList");
         List<Producer> producerList = new ArrayList<>();
         Random random = new Random();
 
--- app/G9/safonova.nastia/lab4/src/main/java/ru/vsu/amm/java/CoffeeService.java
+++ app/G9/safonova.nastia/lab4/src/main/java/ru/vsu/amm/java/CoffeeService.java
@@ -4,12 +4,19 @@ package ru.vsu.amm.java;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class CoffeeService {
+    private static final Logger log;
+
+    static {
+        log = Logger.getLogger(CoffeeService.class.getName());
+    }
 
     public static Map<ProcessingType, List<Sort>> getSortsByProcessingType
             (List<Producer> producers){
+        log.info("Method getSortsByProcessingType");
         return producers.stream()
                 .filter(producer -> producer.processingType() != null && producer.sort() != null)
                 .collect(Collectors.groupingBy(
@@ -24,6 +31,7 @@ public class CoffeeService {
     }
 
     public static Set<String> getCountriesWithHighAltitude(List<Producer> producers){
+        log.info("Method getCountriesWithHighAltitude");
         return producers.stream()
                 .filter(info -> info.altitude() > 1500)
                 .map(Producer::country)
@@ -31,6 +39,7 @@ public class CoffeeService {
     }
 
     public static Map<String, Long> getCoffeeCountByFarm(List<Producer> producers) {
+        log.info("Method getCoffeeCountByFarm");
         return producers.stream()
                 .collect(Collectors.groupingBy(
                         Producer::farm,
