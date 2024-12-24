e1aa8d378d1c9a5aeda76f00d15d509a5dbfbd74
missllizavet <liza.nikitina2018@yandex.ru>
2024-12-24 20:08:23 +0300

    Nikitina lab4

--- app/G9/nikitina.elizaveta/lab3/src/main/java/ru/vsu/amm/java/Main.java
+++ app/G9/nikitina.elizaveta/lab3/src/main/java/ru/vsu/amm/java/Main.java
@@ -10,5 +10,17 @@ public class Main {
 
         PrizeGenerator.populatePrizeStorage(storage, 100);
         storage.printRecipients();
+
+        // 1. Найти отдел (отделы), работники которого выигрывали чаще всего
+        System.out.println("Отделы, которые выигрывали чаще всего:");
+        storage.printMostRecurringGroup();
+
+        // 2. Список работников, которые выигрывали премию
+        System.out.println("\nРаботники, которые выигрывали премию:");
+        storage.printRecipients();
+
+        // 3. Узнать количество работников, которые становились лучшими, только 1 раз
+        System.out.println("\nКоличество работников, которые выигрывали только 1 раз:");
+        storage.printUniqueRecipientCount();
     }
 }
