1da7551d5db4b7fff9fc32a1b8e133051d3790b1
Necrobos <mishagavrilow@gmai.com>
2024-12-26 20:50:30 +0300

    4 lab Last push

--- app/G9/gavrilov.mihail/lab4/src/main/java/ru/vsu/amm/java/main/Main.java
+++ app/G9/gavrilov.mihail/lab4/src/main/java/ru/vsu/amm/java/main/Main.java
@@ -10,9 +10,9 @@ public class Main {
     private static final String PATH = "C:/Users/777/IdeaProjects/amm-java-9-91-2024" +
             "/app/G9/gavrilov.mihail/lab4/src/main/java/ru/vsu/amm/java/main/Task.txt";
 
-    private static final String CONFESSION_COUNT_PER_INSTRUMENT = "Страны с лучшим медальным зачетом ";
-    private static final String SUMMARY_DURATION = "Атлеты с видом спортом и медалью ";
-    private static final String NO_CONFESSION = " Атлеты с максимальным количеством медалей";
+    private static final String COUNTRY_WITH_MEDALS_3 = "Страны с лучшим медальным зачетом ";
+    private static final String ATHLETES_WITH_SPORTS = "Атлеты с видом спортом и медалью ";
+    private static final String ATHLETES_WITH_MAX_MEDALS = " Атлеты с максимальным количеством медалей";
 
     public static void main(String[] args) {
 
@@ -21,8 +21,9 @@ public class Main {
         List<OlympicMedalsRecord> entities = reader.read(PATH);
 
 
-        System.out.println(CONFESSION_COUNT_PER_INSTRUMENT + service.TopMedalCountryResult(entities));
-        System.out.println(SUMMARY_DURATION + service.AthletesWithMedalsAndSport(entities).toString());
-        System.out.println(NO_CONFESSION + service.AthleteWithMaxMedals(entities));
+        System.out.println(COUNTRY_WITH_MEDALS_3 + service.TopMedalCountryResult(entities));
+        System.out.println(ATHLETES_WITH_SPORTS + service.AthletesWithMedalsAndSport(entities).toString());
+        System.out.println(ATHLETES_WITH_MAX_MEDALS + service.AthleteWithMaxMedals(entities));
     }
+
 }
