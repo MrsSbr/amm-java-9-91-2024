92c6e8cd01bd4b0de2291da4a6b664c5e0f651c0
Necrobos <mishagavrilow@gmai.com>
2024-12-26 19:53:25 +0300

    4 lab FileReaderTestNewTest

--- app/G9/gavrilov.mihail/lab4/src/test/java/FileReaderTest.java
+++ app/G9/gavrilov.mihail/lab4/src/test/java/FileReaderTest.java
@@ -12,4 +12,11 @@ public class FileReaderTest {
     public void testUnCorrectPath() {
         assertEquals(fileReader.read("abc"), new ArrayList<>());
     }
+
+    @Test
+    public void testUnCorrectPath2() {
+        assertEquals(fileReader.read("C:/Users/777/IdeaProjects/amm-java-9-91-2024/app" +
+                "/G9/gavrilov.mihail/lab4/src/main/java/ru/vsu/amm/java/main/test.txt"),
+                new ArrayList<>());
+    }
 }
