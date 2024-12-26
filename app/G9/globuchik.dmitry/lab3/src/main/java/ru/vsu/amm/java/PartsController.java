b72e37cd24cd417eec0e43539db3471de120f49d
ozeiroz <fursovgggg@gmail.com>
2024-12-26 14:57:25 +0300

    Updated Logic

--- app/G9/globuchik.dmitry/lab3/src/main/java/ru/vsu/amm/java/PartsController.java
+++ app/G9/globuchik.dmitry/lab3/src/main/java/ru/vsu/amm/java/PartsController.java
@@ -1,17 +1,22 @@
 package ru.vsu.amm.java;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+
+import static ru.vsu.amm.java.DetailsService.*;
 
 public class PartsController {
 
     private static final int PARTS_COUNT = 1570;
 
     public static void main(String[] args) {
-        DetailsList list = new DetailsList(PARTS_COUNT);
-        //list.print();
-        List<String> detailsCount = list.countDetails();
-        List<DetailTypes> detailTypes = list.getUniqueDetailTypes();
+        List<Part> list = new LinkedList<>();
+        generateList(PARTS_COUNT, list);
+        print(list);
+        List<String> detailsCount = countDetails(list);
+        Set<DetailTypes> detailTypes = getUniqueDetailTypes(list);
         System.out.println("Details types: ");
         for (DetailTypes detailType : detailTypes) {
             System.out.println(detailType);
--- app/G9/globuchik.dmitry/lab3/src/test/java/ru/vsu/amm/java/DetailsListTest.java
+++ app/G9/globuchik.dmitry/lab3/src/test/java/ru/vsu/amm/java/DetailsListTest.java
@@ -6,46 +6,45 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static ru.vsu.amm.java.DetailTypes.*;
+import static ru.vsu.amm.java.DetailsService.*;
 
 class DetailsListTest {
 
-    private DetailsList detailsList;
+    private final List<Part> detailsList = new LinkedList<>();
 
     @BeforeEach
     void setup() {
 
-        detailsList = new DetailsList(0);
         Random rand = new Random();
         int count = rand.nextInt(1000) + 1;
-
         for (int i = 0; i < count; ++i) {
-            detailsList.addPart(DetailTypes.getRandomType());
+            addPart(DetailTypes.getRandomType(), detailsList);
         }
 
     }
 
     @org.junit.jupiter.api.Test
     void getPartsTest() {
-        assertNotNull(detailsList.getParts());
-        assertFalse(detailsList.getParts().isEmpty());
+        assertNotNull(detailsList);
+        assertFalse(detailsList.isEmpty());
     }
 
     @org.junit.jupiter.api.Test
     void addPartTest() {
-        detailsList.addPart(DetailTypes.BOLT);
-
-        List<Part> parts = detailsList.getParts();
-        assertTrue(parts.contains(new Part(DetailTypes.BOLT)));
+        addPart(DetailTypes.BOLT, detailsList);
+        assertTrue(detailsList.contains(new Part(DetailTypes.BOLT)));
     }
 
     @Test
     void getUniqueDetailsTest() {
-        List<DetailTypes> uniqueTypes = detailsList.getUniqueDetailTypes();
+        Set<DetailTypes> uniqueTypes = getUniqueDetailTypes(detailsList);
 
         assertNotNull(uniqueTypes);
         assertFalse(uniqueTypes.isEmpty());
@@ -58,7 +57,7 @@ class DetailsListTest {
 
     @Test
     void countDetailsTest() {
-        List<String> counts = detailsList.countDetails();
+        List<String> counts = countDetails(detailsList);
 
         assertNotNull(counts);
         assertFalse(counts.isEmpty());
