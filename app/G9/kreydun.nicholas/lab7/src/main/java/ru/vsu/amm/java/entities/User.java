d97d0d4a667aeffcd6b41f4dfe529c096e9e6d22
nichlazzz <koley-2015@mail.ru>
2025-02-13 20:52:51 +0300

    added db entities

---
+++ app/G9/kreydun.nicholas/lab7/src/main/java/ru/vsu/amm/java/entities/User.java
@@ -0,0 +1,46 @@
+package ru.vsu.amm.java.entities;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class User {
+
+    private UUID id;
+    private String username;
+    private String email;
+    private String password;
+    private Timestamp createdAt;
+
+    public User() {}
+
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
