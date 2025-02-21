08da62836e8f4abd1e9eda43d23efa3fdb4ae288
Oleg Nelezin <astravsu@gmail.com>
2025-02-21 14:20:49 +0300

    feat: add db script and index.jsp

--- app/G9/nelezin.oleg/lab7/build.gradle.kts
+++ app/G9/nelezin.oleg/lab7/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("javax.servlet:javax.servlet-api:4.0.1")
+    implementation("javax.servlet.jsp:javax.servlet.jsp-api:2.3.3")
+    implementation("javax.servlet:jstl:1.2")
 }
 
 tasks.test {
