74c91941d4f6095efb38b0fe01b9458d9471abea
Oleg Nelezin <astravsu@gmail.com>
2025-02-22 11:39:01 +0300

    feat: wrote test variant of currency repository

--- app/G9/nelezin.oleg/lab7/src/main/java/ru/vsu/amm/java/repository/CurrencyRepository.java
+++ app/G9/nelezin.oleg/lab7/src/main/java/ru/vsu/amm/java/repository/CurrencyRepository.java
@@ -1,9 +1,14 @@
 package ru.vsu.amm.java.repository;
 
 import ru.vsu.amm.java.configuration.DatabaseConfiguration;
+import ru.vsu.amm.java.entity.Currency;
 
 import javax.sql.DataSource;
-import java.util.Currency;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,22 +21,63 @@ public class CurrencyRepository implements CrudRepository<Currency> {
     }
 
     @Override
-    public Optional<Currency> findById(Long id) {
+    public Optional<Currency> findById(Long id) throws SQLException {
+        final String query = "SELECT * FROM currency WHERE id = ?";
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setLong(1, id);
+        preparedStatement.execute();
+
+        ResultSet resultSet = preparedStatement.getResultSet();
+        if (resultSet.next()) {
+            return Optional.of(new Currency(
+                    resultSet.getLong("id"),
+                    resultSet.getString("code"),
+                    resultSet.getString("name"),
+                    resultSet.getString("sign")
+            ));
+        }
+
         return Optional.empty();
     }
 
     @Override
-    public List<Currency> findAll() {
-        return null;
+    public List<Currency> findAll() throws SQLException {
+        final String query = "SELECT * FROM currency";
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.execute();
+
+        ResultSet resultSet = preparedStatement.getResultSet();
+        List<Currency> currencies = new ArrayList<>();
+        while (resultSet.next()) {
+            currencies.add(new Currency(
+                    resultSet.getLong("id"),
+                    resultSet.getString("code"),
+                    resultSet.getString("name"),
+                    resultSet.getString("sign")
+            ));
+        }
+        return currencies;
     }
 
-    public Optional<Currency> findByCode(String code) {
+    public Optional<Currency> findByCode(String code) throws SQLException {
+        final String query = "SELECT * FROM currency WHERE code = ?";
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, code);
+        preparedStatement.execute();
+
+        ResultSet resultSet = preparedStatement.getResultSet();
+        if (resultSet.next()) {
+            return Optional.of(new Currency(
+                    resultSet.getLong("id"),
+                    resultSet.getString("code"),
+                    resultSet.getString("name"),
+                    resultSet.getString("sign")
+                    ));
+        }
         return Optional.empty();
-    }
-
-    @Override
-    public void update(Currency entity) {
-
     }
 
     @Override
@@ -41,6 +87,11 @@ public class CurrencyRepository implements CrudRepository<Currency> {
 
     @Override
     public void delete(Currency entity) {
+
+    }
+
+    @Override
+    public void update(Currency entity) {
 
     }
 }
