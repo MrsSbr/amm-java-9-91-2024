e9fbef880d77afba4a8eea2eb0a5b2ada287e003
AlexSpeal <alexspeal@gmail.com>
2025-03-05 20:45:54 +0300

    some fix

--- app/G91/gonnykh.alexander/lab7/src/main/resources/database/InitMigration.sql
+++ app/G91/gonnykh.alexander/lab7/src/main/resources/database/InitMigration.sql
@@ -7,21 +7,15 @@ CREATE TABLE car
     status TEXT NOT NULL
 );
 
-CREATE INDEX idx_car_id ON car (id);
-
 CREATE TABLE users
 (
     id            BIGSERIAL PRIMARY KEY,
     username      TEXT NOT NULL,
     hash_password TEXT NOT NULL,
-    email         TEXT NOT NULL
+    email         TEXT NOT NULL,
+    CONSTRAINT users_username_unique UNIQUE (username),
+    CONSTRAINT users_email_unique UNIQUE (email)
 );
-
-CREATE INDEX idx_users_id ON users (id);
-
-ALTER TABLE users
-    ADD CONSTRAINT users_username_unique UNIQUE (username),
-    ADD CONSTRAINT users_email_unique UNIQUE (email);
 
 CREATE TABLE user_car
 (
@@ -30,13 +24,10 @@ CREATE TABLE user_car
     car_id           BIGINT         NOT NULL,
     start_trip       TIMESTAMP      NOT NULL,
     duration         INTEGER,
-    price_per_minute DECIMAL(10, 2) NOT NULL
+    price_per_minute DECIMAL(10, 2) NOT NULL,
+    CONSTRAINT user_car_user_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT user_car_car_fk FOREIGN KEY (car_id) REFERENCES car(id) ON DELETE CASCADE
 );
 
-CREATE INDEX idx_user_car_id ON user_car (id);
 CREATE INDEX idx_user_car_user_id ON user_car (user_id);
 CREATE INDEX idx_user_car_car_id ON user_car (car_id);
-
-ALTER TABLE user_car
-    ADD CONSTRAINT user_car_user_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    ADD CONSTRAINT user_car_car_fk FOREIGN KEY (car_id) REFERENCES car(id) ON DELETE CASCADE;
