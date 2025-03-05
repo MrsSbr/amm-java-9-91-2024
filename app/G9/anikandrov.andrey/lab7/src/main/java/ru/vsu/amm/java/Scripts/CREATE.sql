6d0146c4b02b61d268789287ed6bd5716cf86042
aanikandrov <139365573+aanikandrov@users.noreply.github.com>
2025-03-05 20:58:42 +0300

    Update CREATE.sql

--- app/G9/anikandrov.andrey/lab7/src/main/java/ru/vsu/amm/java/Scripts/CREATE.sql
+++ app/G9/anikandrov.andrey/lab7/src/main/java/ru/vsu/amm/java/Scripts/CREATE.sql
@@ -1,26 +1,26 @@
 
 CREATE TABLE UserTable (
        UserID SERIAL PRIMARY KEY,
-       UserName VARCHAR(200) NOT NULL UNIQUE,
-       Password VARCHAR(30) NOT NULL,
-       UserRole VARCHAR(50),
+       UserName VARCHAR(30) NOT NULL UNIQUE,
+       Password VARCHAR(50) NOT NULL,
+       UserRole VARCHAR(5),
        Phone VARCHAR(11) UNIQUE,
        BirthDate DATE
 );
 
 CREATE TABLE RentalObjectTable (
         ObjectID SERIAL PRIMARY KEY,
-        ObjectName VARCHAR(200) NOT NULL,
+        ObjectName VARCHAR(100) NOT NULL,
         ObjectType VARCHAR(10) NOT NULL,
         ObjectInfo TEXT,
-        Price INT -- rent price in rubles (for day)
+        Price INTEGER -- rent price in rubles (for day)
 );
 
 CREATE TABLE AgreementTable (
         AgreementID SERIAL PRIMARY KEY,
         UserID INTEGER REFERENCES UserTable (UserID),
         ObjectID INTEGER REFERENCES RentalObjectTable (ObjectID),
-        EventName VARCHAR(200) NOT NULL,
+        EventName VARCHAR(100) NOT NULL,
         EventInfo TEXT,
         TimeBegin DATE,
         TimeEnd DATE,
