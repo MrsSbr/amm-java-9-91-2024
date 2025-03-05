a61dd937dc7637ae58d348d14a1e260e877e2303
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2025-03-05 21:03:43 +0300

    golukovich lab7: dto, model-view update

--- app/G9/golukovich.timofey/lab7/src/main/java/ru/vsu/amm/java/entities/EmployeeEntity.java
+++ app/G9/golukovich.timofey/lab7/src/main/java/ru/vsu/amm/java/entities/EmployeeEntity.java
@@ -3,15 +3,103 @@ package ru.vsu.amm.java.entities;
 import java.util.Date;
 
 public class EmployeeEntity {
-    public int id;
-    public int hotelId;
-    public String login;
-    public String password;
-    public String name;
-    public String phoneNumber;
-    public String email;
-    public String passportNumber;
-    public String passportSeries;
-    public String post;
-    public int salary;
+    private int id;
+    private int hotelId;
+    private String login;
+    private String password;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String passportNumber;
+    private String passportSeries;
+    private String post;
+    private int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public String getPassportSeries() {
+        return passportSeries;
+    }
+
+    public void setPassportSeries(String passportSeries) {
+        this.passportSeries = passportSeries;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
--- app/G9/golukovich.timofey/lab7/src/main/java/ru/vsu/amm/java/entities/HotelEntity.java
+++ app/G9/golukovich.timofey/lab7/src/main/java/ru/vsu/amm/java/entities/HotelEntity.java
@@ -1,12 +1,60 @@
 package ru.vsu.amm.java.entities;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class HotelEntity {
-    public int id;
-    public String name;
-    public String address;
-    public String email;
-    public String phoneNumber;
-    public Date openingDate;
+    private int id;
+    private String name;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime openingDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getOpeningDate() {
+        return openingDate;
+    }
+
+    public void setOpeningDate(LocalDateTime openingDate) {
+        this.openingDate = openingDate;
+    }
 }
