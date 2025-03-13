f9e4862459cb0c961046f7a332a5d8fef71012c3
Ulyanov Leonid <ulyanov.leo.v@gmail.com>
2025-03-13 20:11:59 +0300

    added lombok

--- app/G91/ulyanov.leonid/lab7/build.gradle.kts
+++ app/G91/ulyanov.leonid/lab7/build.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    compileOnly 'org.projectlombok:lombok:1.18.20'
-    annotationProcessor 'org.projectlombok:lombok:1.18.20'
+    compileOnly("org.projectlombok:lombok:1.18.20")
+    annotationProcessor("org.projectlombok:lombok:1.18.20")
 }
 
 tasks.test {
--- app/G91/ulyanov.leonid/lab7/src/main/java/ru/vsu/amm/java/entities/BookUpdates.java
+++ app/G91/ulyanov.leonid/lab7/src/main/java/ru/vsu/amm/java/entities/BookUpdates.java
@@ -1,57 +1,31 @@
 package ru.vsu.amm.java.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import ru.vsu.amm.java.enums.UpdateType;
+
 import java.time.LocalDateTime;
 
+@Getter
+@AllArgsConstructor
 public class BookUpdates {
+    @Setter
     private Integer updateId;
+    @Setter
     private Integer userId;
+    @Setter
     private Integer bookId;
     private LocalDateTime updateTime;
+    @Setter
     private UpdateType updateType;
 
     public BookUpdates() {}
-
-    public void setUpdateId(Integer updateId) {
-        this.updateId = updateId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public void setBookId(Integer bookId) {
-        this.bookId = bookId;
-    }
 
     public void setUpdateTime(LocalDateTime updateTime) {
         if (updateTime.isAfter(LocalDateTime.now())) {
             throw new IllegalArgumentException("Update time is after now");
         }
         this.updateTime = updateTime;
-    }
-
-    public void setUpdateType(UpdateType updateType) {
-        this.updateType = updateType;
-    }
-
-    public Integer getUpdateId() {
-        return updateId;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public Integer getBookId() {
-        return bookId;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public UpdateType getUpdateType() {
-        return updateType;
     }
 }
