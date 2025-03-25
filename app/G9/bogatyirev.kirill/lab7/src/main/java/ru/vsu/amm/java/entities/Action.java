40aabd095e1cff53fd2604daccaaa0c167e45dbe
NonsencePluto <kirill210804@mail.ru>
2025-03-25 23:05:46 +0300

    model, SQL, entities commit

---
+++ app/G9/bogatyirev.kirill/lab7/src/main/java/ru/vsu/amm/java/entities/Action.java
@@ -0,0 +1,7 @@
+package ru.vsu.amm.java.entities;
+
+public class Action {
+    private int id;
+    private String name;
+    private int points;
+}
---
+++ app/G9/bogatyirev.kirill/lab7/src/main/java/ru/vsu/amm/java/entities/Player.java
@@ -0,0 +1,8 @@
+package ru.vsu.amm.java.entities;
+
+public class Player {
+    private Long id;
+    private String login;
+    private String password;
+    private String email;
+}
