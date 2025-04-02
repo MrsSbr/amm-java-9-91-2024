60c6a045b0444840e52c2b71fd8f62b6d062795e
aanikandrov <139365573+aanikandrov@users.noreply.github.com>
2025-04-02 11:36:22 +0300

    Update build.gradle.kts

--- app/G9/anikandrov.andrey/lab7/build.gradle.kts
+++ app/G9/anikandrov.andrey/lab7/build.gradle.kts
@@ -11,16 +11,19 @@ repositories {
 }
 
 tasks.war {
-    archiveFileName.set("ROOT.war")
+    archiveFileName.set("anikandrov-andrey-lab7.war")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.postgresql:postgresql:42.7.3")
     implementation("javax.servlet:javax.servlet-api:4.0.1")
     implementation("javax.servlet.jsp:javax.servlet.jsp-api:2.3.3")
     implementation("javax.servlet:jstl:1.2")
+    implementation("org.postgresql:postgresql:42.7.3")
+    implementation("org.mindrot:jbcrypt:0.4")
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
 }
 
 tasks.test {
