b9e743ea3cb029931e6b3dc074f44c2088589a76
AlexSpeal <alexspeal@gmail.com>
2025-04-03 19:51:18 +0300

    fix imports

--- app/G91/gonnykh.alexander/lab7/src/main/java/ru/vsu/amm/java/servlet/RegisterServlet.java
+++ app/G91/gonnykh.alexander/lab7/src/main/java/ru/vsu/amm/java/servlet/RegisterServlet.java
@@ -1,17 +1,17 @@
 package ru.vsu.amm.java.servlet;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import ru.vsu.amm.java.exceptions.DataAccessException;
 import ru.vsu.amm.java.exceptions.WrongUserCredentialsException;
 import ru.vsu.amm.java.model.requests.RegisterRequest;
 import ru.vsu.amm.java.service.implementations.DefaultAuthService;
 import ru.vsu.amm.java.service.interfaces.AuthService;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "RegisterServlet", urlPatterns = "/register")
