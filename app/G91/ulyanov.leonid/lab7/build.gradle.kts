1960ca3c57dd8022869ad59991c30d9f78631725
leonid <ulyanov.leo.v@gmail.com>
2025-04-09 01:18:46 +0300

    chore: refactored folder name, changed lombok version

--- app/G91/ulyanov.leonid/lab7/build.gradle.kts
+++ app/G91/ulyanov.leonid/lab7/build.gradle.kts
@@ -18,8 +18,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    compileOnly("org.projectlombok:lombok:1.18.20")
-    annotationProcessor("org.projectlombok:lombok:1.18.20")
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
     implementation("org.postgresql:postgresql:42.7.5")
     implementation ("org.apache.logging.log4j:log4j-api:2.20.0")
     implementation ("org.apache.logging.log4j:log4j-core:2.20.0")
