8a8822b7acea8f8610c483f2a97268d4f48bc16d
ozeiroz <fursovgggg@gmail.com>
2025-04-12 02:04:38 +0300

    Added  Tomcat

--- app/G9/globuchik.dmitry/lab7/build.gradle.kts
+++ app/G9/globuchik.dmitry/lab7/build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     id("java")
+    id("war")
+}
+
+tasks.war {
+    archiveFileName.set("lab7.war")
 }
 
 group = "ru.vsu.amm.java"
@@ -12,6 +17,13 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("javax.servlet:javax.servlet-api:4.0.1")
+    implementation("javax.servlet.jsp:javax.servlet.jsp-api:2.3.3")
+    implementation("javax.servlet:jstl:1.2")
+    implementation("org.postgresql:postgresql:42.7.3")
+    implementation("org.mindrot:jbcrypt:0.4")
+    compileOnly("org.projectlombok:lombok:1.18.20")
+    annotationProcessor("org.projectlombok:lombok:1.18.20")
 }
 
 tasks.test {
---
+++ app/G9/globuchik.dmitry/lab7/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Lab7</title>
+</head>
+<body>
+<h1>Hello from JSP!</h1>
+</body>
+</html>
