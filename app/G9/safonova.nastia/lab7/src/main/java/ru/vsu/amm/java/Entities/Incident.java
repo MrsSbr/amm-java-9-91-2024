64715a056814c0d41cc41d33c2642693083f4ebe
Nastia Saf <safonovaanastasia2004@gmail.com>
2025-04-13 18:17:50 +0300

    Tomcat connection and fixes

--- app/G9/safonova.nastia/lab7/src/main/java/ru/vsu/amm/java/Entities/Incident.java
+++ app/G9/safonova.nastia/lab7/src/main/java/ru/vsu/amm/java/Entities/Incident.java
@@ -7,17 +7,16 @@ public class Incident {
     private long emplId;
     private long dinoId;
     private LocalDate dateOfIncident;
-
     private String discription;
 
     public Incident() {}
 
-    public Incident(LocalDate dateOfIncident, long dinoId, long emplId, long idIncident, String discription) {
+    public Incident(long idIncident, long emplId, long dinoId, LocalDate dateOfIncident, String discription) {
+        this.idIncident = idIncident;
+        this.emplId = emplId;
+        this.dinoId = dinoId;
         this.dateOfIncident = dateOfIncident;
         this.discription = discription;
-        this.dinoId = dinoId;
-        this.emplId = emplId;
-        this.idIncident = idIncident;
     }
 
     public long getIdIncident() {
