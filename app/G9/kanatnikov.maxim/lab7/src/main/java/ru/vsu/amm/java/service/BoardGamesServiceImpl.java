eacdc5a91912fe4be862ed1698decc5baa6e54c8
kanatnikovmaxim@yandex.ru <kanatnikovmaxim@yandex.ru>
2025-04-14 21:01:40 +0300

    remove extra import

--- app/G9/kanatnikov.maxim/lab7/src/main/java/ru/vsu/amm/java/service/BoardGamesServiceImpl.java
+++ app/G9/kanatnikov.maxim/lab7/src/main/java/ru/vsu/amm/java/service/BoardGamesServiceImpl.java
@@ -7,7 +7,6 @@ import ru.vsu.amm.java.service.interfaces.BoardGamesService;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
